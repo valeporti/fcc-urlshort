ba3af196d545307b66e497c8341172a41f6d4308
Valentín Portillo Galván <valeporti@hotmail.com>
2017-04-11 04:29:32 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Request Header Parser Microservice API
+Free Code Camp APIs URL Shortener Microservice
+
+## What is this?
+This is a microservice API project for Free Code Camp that is intended to return the information of the url requested.
+
+If the url does exist, it shows the "short number" of the url.
+
+If the url doesn't exist, it assigns a "short number" and the shows you the result of the url and its "short number" associated.
+
+If the url is a number associated to an url, it sends you to the respective associated url.
+
+* If you pass a valid url without https or http, it will assign automatically an https to the beginning
+
+## User stories:
+1) I can pass a URL as a parameter and I will receive a shortened URL in the JSON response.
+
+2) If I pass an invalid URL that doesn't follow the valid http://www.example.com format, the JSON response will contain an error instead.
+
+3) When I visit that shortened URL, it will redirect me to my original link.
+
+## Usage:
+Wrong Entry:
+`https://urlshort-vp.herokuapp.com/htts//:good432`
+
+Right Entries:
+`https://urlshort-vp.herokuapp.com/https://www.google.com`
+`https://urlshort-vp.herokuapp.com/www.google.com`
+`https://urlshort-vp.herokuapp.com/8`     ** as a short URL **
+
+## Example Response:
+Error response from Wrong Entry:
+`{"error":"Sorry, wrong entry : 'htts//:good432', please try another Url"}`
+Response from Right Entries:
+`{"long_url":"https://www.google.com","short_url":8}`
+`Or... Directly to the requested url page`
---
+++ server.js
@@ -0,0 +1,87 @@
+var express = require('express');
+var app = express();
+var mod = require('./module');
+
+app.use(express.static('public'));
+app.use(express.static('views'));
+
+//using MongoDB and deploying to Herok reference
+//https://community.c9.io/t/setting-up-mongodb/1717
+//https://github.com/FreeCodeCamp/FreeCodeCamp/wiki/Using-MongoDB-And-Deploying-To-Heroku
+// mongod --port 27017 --dbpath=./data
+// Now your Db is running at- mongodb://localhost:27017/my_database_name
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+    console.log('Node.js listening on port ' + port);
+});
+
+//if 
+app.get("/", function(req, res) {
+    console.log("pasó por /");
+    //http://stackoverflow.com/questions/17014384/how-to-render-multiple-ejs-files-in-nested-form-in-node-js-and-express
+    res.render('index.html');
+   // mod.render(res, 'views', "index.html", []);
+});
+
+app.get("*", function(req, res) {
+    var usageReq = false;
+    var urlPassed = req.url;
+    urlPassed = urlPassed.substring(1, urlPassed.length); //remove first "/"
+    
+    /*
+    if(urlPassed.length > 6 && urlPassed.substring(0, 5) == "usage") { //seee if usage asked in order to render answer
+        //console.log("detectó usage");
+        usageReq = true;
+        urlPassed = urlPassed.substring(6, urlPassed.length) 
+        
+    }
+    */
+    
+    //console.log(urlPassed);
+    var expected = mod.seeIfExpected(urlPassed); //returns url(if url) or num(if number) or na(if it doesn't match any)
+    //console.log(expected);
+    mod.processInfo(expected, urlPassed, function(answer) {
+        console.log("showing answer");
+        console.log(answer);
+        if (answer.found == false && answer.jsonRes == true && answer.info != "inserted") {
+            res.json({
+                "error": answer.info   
+            });
+        } else {
+            
+            /*
+            if (usageReq == true) {
+                //do something different, if entered number, go to page associated, else render and link strings
+                if (answer.found == false) { //means it was an insertion, lets render answer and link it
+                //http://stackoverflow.com/questions/10919650/accessing-express-js-local-variables-in-client-side-javascript
+                    res.render('render.html', answer);
+                
+                } else { //means it was found
+                    res.render('render.html', answer);
+                }
+            } else {
+                */
+                if (answer.jsonRes == true) {
+                    res.json({
+                       "long_url": answer.url,
+                       "short_url": answer.id
+                    });
+                } else { // go to url
+                    //if(/https/g.test(answer.url)) {
+                        res.redirect(answer.url);
+                    /*} else {
+                        console.log("le falta https");
+                        var newUrl = "https://" + answer.url;
+                        console.log(newUrl);
+                        res.redirect(newUrl);
+                    }*/
+                }
+            //}
+        }
+    });
+});
+
+
+
+
